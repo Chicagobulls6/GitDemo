f78fe459b4a51431ff7ff2c857de3f744b3a99d4
devarsh <devarshdpatel@fmail.com>
2021-12-17 15:53:52 -0600

    First commit

---
+++ src/test/java/Mavenjava/qaautomation/APITest.java
@@ -0,0 +1,21 @@
+package Mavenjava.qaautomation;
+
+import static org.junit.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+public class APITest {
+	
+	@Test
+	public void postJira()
+	{
+		assertTrue( true );
+		System.out.println("postJira");
+	}
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+	}
+
+}
---
+++ src/test/java/Mavenjava/qaautomation/SeleniumTest.java
@@ -0,0 +1,19 @@
+package Mavenjava.qaautomation;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	// testing xml file from Maven
+	//how to execute all test cases from Test Folder - mvn test
+	@Test
+	public void browserAutomation()
+	{
+		System.out.println("browserAutomation");
+	}
+	@Test
+	public void elementUi()
+	{
+		System.out.println("elementUi");
+	}
+
+}
